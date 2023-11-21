5e5735e1546a3f1b911c8f5dd82057471a1c76c8
losada.diana@gmail.com <losada.diana@gmail.com>
2023-11-21 14:38:41 +0100

    SUP-5 Caching searching requests in memory
    Created CacheConfig,
    Cacheable getSuperheroes and getSuperheroesByName

--- src/main/java/com/superhero/superheroservice/service/SuperheroService.java
+++ src/main/java/com/superhero/superheroservice/service/SuperheroService.java
@@ -2,16 +2,21 @@ package com.superhero.superheroservice.service;
 
 import com.superhero.superheroservice.model.Superhero;
 import com.superhero.superheroservice.repository.SuperheroRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
+@Slf4j
 public class SuperheroService {
     @Autowired
     private SuperheroRepository superheroRepository;
-
+    @Cacheable(cacheNames="superheroesCache")
     public List<Superhero> getAllSuperheroes() {
+        log.info("getAllSuperheroes called");
         return superheroRepository.findAll();
     }
 
@@ -19,14 +24,18 @@ public class SuperheroService {
         return superheroRepository.findById(id).orElse(null);
     }
 
+    @Cacheable(cacheNames="superheroesCache")
     public List<Superhero> getSuperheroesByName(String name) {
+        log.info("getSuperheroesByName called");
         return superheroRepository.findByNameIgnoreCaseContaining(name);
     }
 
+    @CacheEvict(cacheNames="superheroesCache", allEntries=true)
     public Superhero createSuperhero(Superhero superhero) {
         return superheroRepository.save(superhero);
     }
 
+    @CacheEvict(cacheNames="superheroesCache", allEntries=true)
     public Superhero updateSuperhero(Long id, Superhero superhero) {
         if (superheroRepository.existsById(id)) {
             superhero.setName(superhero.getName());
@@ -35,6 +44,7 @@ public class SuperheroService {
         return null;
     }
 
+    @CacheEvict(cacheNames="superheroesCache", allEntries=true)
     public void deleteSuperhero(Long id) {
         superheroRepository.deleteById(id);
     }
